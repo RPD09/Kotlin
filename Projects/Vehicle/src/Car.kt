5449b57569c3e7f871cad1c69b2ccacb5c8836cd
Rafael Dias <66210711+RPD09@users.noreply.github.com>
2024-10-16 15:45:29 +0100

    Delete Projects/Vehicle/src directory

--- Projects/Vehicle/src/Car.kt
+++
@@ -1,5 +0,0 @@
-class Car(brand: String, model: String, year: Int, val numberOfDoors: Int) : Vehicle(brand, model, year), Drivable{
-    override fun drive(){
-        println("Driving the car: $brand, model $model, year $year, with $numberOfDoors doors")
-    }
-}
